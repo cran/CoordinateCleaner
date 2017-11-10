083b85ddf634f52d41cbc6e85e3b7bd5fbed52b8
Alexander Zizka <alexander.zizka@bioenv.gu.se>
2017-11-10 10:47:13 +0000

    version 1.0-2

---
+++ R/CleanCoordinatesDS.R
@@ -0,0 +1,105 @@
+CleanCoordinatesDS <- function(x, lon = "decimallongitude", lat = "decimallatitude", ds = "dataset",
+                               ddmm = TRUE, periodicity = TRUE, 
+                               ddmm.pvalue = 0.025, ddmm.diff = 0.2, 
+                               periodicity.target = "lon_lat", periodicity.thresh = 3.5, 
+                               periodicity.diagnostics = FALSE, 
+                               periodicity.subsampling = NULL, 
+                               value = "dataset", verbose = TRUE) {
+  
+  # check input arguments
+  match.arg(value, choices = c("dataset", "flags", "clean"))
+  match.arg(periodicity.target, choices = c("lat", "lon", "lon_lat"))
+  
+  #check column names
+  nams <- c(lon, lat, ds)
+  if(!all(nams %in% names(x))){
+    stop(sprintf("%s column not found\n", nams[which(!nams %in% names(x))]))
+  }
+
+  # prepare input data
+  dat <- x[,c(lon, lat, ds)]
+  dat[[ds]] <- as.character(dat[[ds]])
+  
+  ## kick out NAs
+  if (sum(!complete.cases(dat)) > 0) {
+    warning(sprintf("Data contains incomplete cases. value set to 'dataset', ignoring %s cases with incomplete data", sum(!complete.cases(dat))))
+    dat <- dat[complete.cases(dat), ]
+    value <- "dataset"
+  }
+  
+  #perpare dummy  value in case value == "clean"
+  if(value == "clean"){
+    value2  <- "flags"
+  }else{
+    value2 <- value
+  }
+
+  if (nrow(dat) == 0) {
+    stop("no complete cases found")
+  }
+  
+  ## create test columns: decimal degrees long and lat
+  dat$lon.test <- abs(dat[[lon]]) - floor(abs(dat[[lon]]))
+  dat$lat.test <- abs(dat[[lat]]) - floor(abs(dat[[lat]]))
+  
+  ## split into seperate datasets
+  test <- split(dat, f = dat[[ds]])
+  
+  # run ddmm to dd.dd conversion error at 0.6 test
+  if (ddmm) {
+    out.t1 <- dc_ddmm(x = dat, lon = lon, lat = lat, ds = ds,
+                      pvalue = ddmm.pvalue, diff = ddmm.diff,
+                      value = value2, verbose = verbose)
+    if(value == "dataset"){
+      names(out.t1) <- c("binomial.pvalue", "perc.difference", "pass.ddmm")
+    }
+  } else {
+    if(value == "dataset"){
+      out.t1 <- data.frame(pass.ddmm = rep(NA, length(test)))
+    }else{
+      out.t1 <- rep(NA, nrow(dat))
+    }
+  }
+  
+  # Run periodicity test
+  if (periodicity) {
+    out.t2 <- dc_round(x = dat, lon = lon, lat = lat, ds = ds,
+                       target = periodicity.target, threshold = periodicity.thresh, 
+                       subsampling = periodicity.subsampling, diagnostics = periodicity.diagnostics, 
+                       value = value2, verbose = verbose)
+
+  
+    }else{
+      if(value == "dataset"){
+        out.t2 <- data.frame(pass.periodicity.com = rep(NA, length(test)),
+                             pass.zero.com = rep(NA, length(test)))
+      }else{
+        out.t2 <- rep(NA, nrow(dat))
+      }
+    }
+  
+  # prepare output
+  if (value == "dataset") {
+    out <- data.frame(out.t1, out.t2)
+    out$summary <- out$pass.ddmm & out$pass.zero.com & out$pass.periodicity.com
+    out <- Filter(function(x) !all(is.na(x)), out)
+  }
+  if (value == "flags") {
+    out <- data.frame(ddmm = out.t1, periodicity = out.t2)
+    out <- Filter(function(x) !all(is.na(x)), out)
+    out$summary <-  Reduce("&", out)
+
+  }
+  if (value == "clean") {
+    out <- data.frame(ddmm = out.t1, periodicity = out.t2)
+    out <- Filter(function(x) !all(is.na(x)), out)
+    out$summary <-  Reduce("&", out)
+    out <- dat[out$summary,]
+  }
+  
+  #return output
+  if(verbose){
+    cat(sprintf("Flagged %s records\n", sum(!out)))
+  }
+ return(out)
+}
---
+++ R/dc_ddmm.R
@@ -0,0 +1,90 @@
+dc_ddmm <- function(x, lon = "decimallongitude", lat = "decimallatitude", ds = "dataset",
+                    pvalue = 0.025, diff = 0.1, mat.size = 1000, 
+                    value = "clean", verbose = TRUE){
+  
+  #check value argument
+  match.arg(value, choices = c("clean", "flags", "dataset"))
+  
+  if(verbose){
+    cat("Testing datasets for dd.mm to dd.dd conversion errors\n")
+  }
+  
+  #prepare dataset for analyses
+  if (sum(!complete.cases(x)) > 0) {
+    warning(sprintf("ignored %s cases with incomplete data", sum(!complete.cases(x))))
+  }
+  
+  #get function data
+  dat <- x[complete.cases(x), ]
+  
+  if (nrow(dat) == 0) {
+    stop("no complete cases found")
+  }
+
+  ## create test columns: decimal degrees long and lat
+  dat$lon.test <- abs(dat[[lon]]) - floor(abs(dat[[lon]]))
+  dat$lat.test <- abs(dat[[lat]]) - floor(abs(dat[[lat]]))
+  
+  # split into seperate datasets
+  test <- split(dat, f = dat[[ds]])
+  
+  # run ddmm to dd.dd conversion test error at 0.6
+  out <- lapply(test, function(k) {
+      ## create test datasets
+      #dat.unique <- k[!duplicated(k[, c("species", "decimallongitude", "decimallatitude")]),]
+      
+      #Assign decimals to a 100x100 matrix for binomial test
+      cl <- ceiling(k[, c("lon.test", "lat.test")] * mat.size)
+      cl$lat.test <- mat.size - cl$lat.test
+      
+      mat <- matrix(ncol = mat.size, nrow = mat.size)
+      mat[cbind(cl$lat.test, cl$lon.test)] <- 1
+      mat[is.na(mat)] <- 0
+      dat.t1 <- mat
+
+      # Binomial test, to see if more values are below 0.6 than expected
+      P_smaller_than_06 <- floor(0.599 * mat.size) * floor(0.599 * mat.size) / mat.size^2 # 0.3481
+      
+      x.ind <- (mat.size - floor(0.599 * mat.size)):mat.size
+      y.ind <- 1:floor(0.599 * mat.size)
+      
+      subt <- dat.t1[x.ind, y.ind]  # subset tbl 
+      p06 <- sum(subt >= 1)
+      pAll <- sum(dat.t1 >= 1)
+      
+      B <- stats::binom.test(p06, pAll, p = P_smaller_than_06, alternative = c("greater"))
+      
+      # P-VALUE
+      v1 <- B$p.value  
+      # PERCENTAGE OF difference from expected
+      v2 <- (B$estimate - P_smaller_than_06)/P_smaller_than_06  
+      
+      # These two thresholds could be changed
+      if (v1 < pvalue & v2 > diff) {
+        flag.t1 <- FALSE
+      } else {
+        flag.t1 <- TRUE
+      }
+      c(round(v1, 4), round(v2, 3), flag.t1)
+    })
+    
+    # Create output objects  
+    ## Reduce output to data.frame
+    out.ds <- do.call("rbind.data.frame", out)
+    rownames(out.ds) <- names(out)
+    names(out.ds) <- c("binomial.pvalue", "perc.difference", "pass")
+    
+    flags <- x[[ds]] %in% rownames(out.ds[out.ds$pass == 1,])
+
+    # return output dependent on value argument
+    if(verbose){
+      cat(sprintf("Flagged %s records\n", sum(!flags)))
+    }
+    
+    switch(value,
+           dataset = return(out.ds),
+           clean = return(x[flags,]),
+           flags = return(flags))
+
+}
+  
